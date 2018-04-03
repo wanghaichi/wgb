2c0eef7da73f9b9137b65fa4759372747a2d2ef7
Liebes <wanghaichi.twtstudio.com>
2018-04-03 15:51:20 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('login', 'AuthController@login');
 
 Route::group(['middleware' => 'authapi',], function () {
     Route::group(['prefix' => 'auth'], function(){
-        Route::post('login', 'AuthController@login');
         Route::post('logout', 'AuthController@logout');
         Route::post('refresh', 'AuthController@refresh');
         Route::post('me', 'AuthController@me');
